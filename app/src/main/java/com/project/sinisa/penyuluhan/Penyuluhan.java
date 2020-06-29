aa189b718db14ec352ac2dbcbc81bb622f3559cb
ocha <khofsyahsiti@gmail.com>
2020-06-29 20:16:21 +0700

    NIM E31180373_SITI KHOFSYAH
    penyuluhan(view data penyuluhan)
    dashboard(hubungi & tentang)
    layout penyuluhan
    layout dashboard(hubungi & tentang)

---
+++ app/src/main/java/com/project/sinisa/penyuluhan/Penyuluhan.java
@@ -0,0 +1,117 @@
+package com.project.sinisa.penyuluhan;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.project.sinisa.R;
+import com.project.sinisa.config.AppController;
+import com.project.sinisa.config.ServerAccess;
+import com.project.sinisa.penyuluhan.adapter.Adapter_Penyuluhan;
+import com.project.sinisa.penyuluhan.model.Penyuluhan_Model;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Penyuluhan extends AppCompatActivity {
+    private Adapter_Penyuluhan adapter;
+    private List<Penyuluhan_Model> list;
+    private RecyclerView listdata;
+    RecyclerView.LayoutManager mManager;
+    FloatingActionButton tambah;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_penyuluhan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        toolbar.setNavigationIcon(R.drawable.back);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        listdata = (RecyclerView) findViewById(R.id.listdata);
+        listdata.setHasFixedSize(true);
+        list = new ArrayList<>();
+        adapter = new Adapter_Penyuluhan(this,(ArrayList<Penyuluhan_Model>) list, this);
+        mManager = new LinearLayoutManager(getBaseContext(),LinearLayoutManager.VERTICAL,false);
+        listdata.setLayoutManager(mManager);
+        listdata.setAdapter(adapter);
+        tambah = findViewById(R.id.tambah);
+        tambah.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getBaseContext(), Pengajuan_Jadwal.class));
+            }
+        });
+        loadJson();
+    }
+    private void loadJson()
+    {
+        Intent data = getIntent();
+        StringRequest senddata = new StringRequest(Request.Method.GET, ServerAccess.PENYULUHAN+"?type=list", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                JSONObject res = null;
+                try {
+                    res = new JSONObject(response);
+                    JSONArray arr = res.getJSONArray("data");
+                    if(arr.length() > 0) {
+                        for (int i = 0; i < arr.length(); i++) {
+                            try {
+                                JSONObject data = arr.getJSONObject(i);
+                                Log.d("pesan", data.getString("nama"));
+                                Penyuluhan_Model md = new Penyuluhan_Model();
+                                md.setKode(data.getString("id"));
+                                md.setNama(data.getString("nama"));
+                                md.setNama_instansi(data.getString("nama_instansi"));
+                                md.setStatus(data.getString("status"));
+                                md.setTanggal_input(data.getString("tanggal_input"));
+                                md.setTanggal_output(data.getString("tanggal_output"));
+                                md.setMateri(data.getString("materi"));
+                                list.add(md);
+                            } catch (Exception ea) {
+                                ea.printStackTrace();
+                                Log.d("pesan", ea.getMessage());
+                            }
+                        }
+                        adapter.notifyDataSetChanged();
+                    }else{
+                        Toast.makeText(Penyuluhan.this, "Data Kosong", Toast.LENGTH_SHORT).show();
+                    }
+                } catch (JSONException e) {
+                    Toast.makeText(Penyuluhan.this, "Data Kosong", Toast.LENGTH_SHORT).show();
+                    Log.d("pesan", "error "+e.getMessage());
+                    e.printStackTrace();
+                }
+            }
+        },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(Penyuluhan.this, "Data Kosong", Toast.LENGTH_SHORT).show();
+                        Log.d("volley", "errornya : " + error.getMessage());
+                    }
+                });
+        AppController.getInstance().addToRequestQueue(senddata);
+    }
+}
