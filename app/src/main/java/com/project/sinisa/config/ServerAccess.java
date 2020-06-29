5b739acf3f015f98366bc63b5ec77f7acca4f284
nurinastin <57513002+nurinastin@users.noreply.github.com>
2020-06-29 19:30:14 +0700

    E31180297 Nurin Astin Valida
    PENYULUHAN (adapter, model, pengajuan jadwal) ,
    CONFIG ,
    LAYOUT(pengajuan jadwal)

--- app/src/main/java/com/project/sinisa/config/ServerAccess.java
+++ app/src/main/java/com/project/sinisa/config/ServerAccess.java
@@ -10,7 +10,7 @@ public class ServerAccess {
 //    public static final String BASE_URL = "http://192.168.1.8/sinisa/";
     public static final String ROOT_API = BASE_URL+"api/";
     public static final String LOGIN = ROOT_API+"login.php";
-    public static final String REGISTER = ROOT_API+"register.php";
+    public static final String REGISTER = ROOT_API+"Register.php";
     public static final String PENYULUHAN = ROOT_API+"penyuluhan.php";
     public static final String SEWA = ROOT_API+"sewa.php";
     public static final String BARANG = ROOT_API+"barang.php";
